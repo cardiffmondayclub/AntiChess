562fc9507e1bef9c5eda60cdd5867e9e97435284
John Cowie <john@john-laptop.(none)>
2010-02-15 15:10:36 +0000

    Pawn isMoveValid done

--- src/antichess/Pawn.java
+++ src/antichess/Pawn.java
@@ -14,6 +14,87 @@ public class Pawn extends Piece {
    }
 
    public boolean isMoveValid(Board board, Move move) {
+       int newX = move.newX;
+       int newY = move.newY;
+       int oldX = move.oldX;
+       int oldY = move.oldY;
+
+       int xDiff = move.newX - move.oldX;
+       int yDiff = move.newY - move.oldY;
+       int xDiffAbs = Math.abs(xDiff);
+       int yDiffAbs = Math.abs(yDiff);
+
+       // Capture case
+       if (this.colour == 'w' && board.isMoveCapture(move) )
+       {
+           if(xDiffAbs == 1 && yDiff == 1)
+           {
+               return true;
+           }
+           else
+           {
+               return false;
+           }
+       }
+       else if(this.colour == 'b' && board.isMoveCapture(move) )
+       {
+           if(xDiffAbs == 1 && yDiff == -1)
+           {
+               return true;
+           }
+           else
+           {
+               return false;
+           }
+       }
+
+       // First move case
+       if( this.colour == 'w' && oldY == 1)
+       {
+           if(yDiff <= 2 && yDiff > 0 && xDiff == 0 && board.isPathClear(move) )
+           {
+               return true;
+           }
+           else
+           {
+               return false;
+           }
+       }
+       else if( this.colour == 'b' && oldY == 6)
+       {
+           if(yDiff >= -2 && yDiff < 0 && xDiff == 0 && board.isPathClear(move) )
+           {
+               return true;
+           }
+           else
+           {
+               return false;
+           }
+       }
+       // other case
+       else if(this.colour == 'w')
+       {
+           if (xDiff == 0 && yDiff == 1)
+           {
+               return true;
+           }
+           else
+           {
+               return false;
+           }
+       }
+       else if(this.colour == 'b')
+       {
+           if (xDiff == 0 && yDiff == -1)
+           {
+               return true;
+           }
+           else
+           {
+               return false;
+           }
+       }
+
       return true;
    }
 
