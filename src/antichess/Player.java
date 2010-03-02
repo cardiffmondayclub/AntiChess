59d24c4397d9bb25f72597ad949c3385901b0c1f
John Cowie <john@john-laptop.(none)>
2010-03-02 15:39:16 +0000

    work begins on player class

--- src/antichess/Player.java
+++ src/antichess/Player.java
@@ -1,5 +1,24 @@
 package antichess;
 
-public class Player {
+public abstract class Player {
+   // Instance Variables
+   public static final int WHITE = 0;
+   public static final int BLACK = 1;
 
+   private int playerColour;
+
+   // construct player with variable player colour
+   public Player(boolean starts) {
+      if( starts ) {
+         playerColour = WHITE;
+      }
+      else {
+         playerColour = BLACK;
+      }
+
+   }
+   
+   public int getPlayerColour() {
+      return playerColour;
+   }
 }
